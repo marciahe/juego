ce406b30646a348e79368d3cbeb4bbbc72fd4213
Marcia He <marcia@kubik.mx>
2015-11-09 21:14:01 -0600

    Se agrego un Do While para volver a jugar al terminar

--- js.js
+++ js.js
@@ -1,25 +1,36 @@
-var user = prompt("Para empezar a jugar escribe 1 para elegir piedra, 2 para elegir papel o 3 para elegir tijera.");
-var machine = Math.floor(Math.random() * 3) + 1;
 
-
-if (user == machine){
-	alert("Empataste");
-}
-else if (machine == 1 && user == 2){
-	alert("La compu eligió Piedra, Ganaste!");
-}
-else if (machine == 1 && user == 3){
-	alert("La compu eligió Piedra, Perdiste!");
-}
-else if (machine == 2 && user == 1){
-	alert("La compu eligió Papel, Perdiste.");
-} 
-else if (machine == 2 && user == 3){
-	alert("La compu eligió Papel, Ganaste!");
-}
-else if (machine == 3 && user == 2){
-	alert("La compu eligió Tijeras, Perdiste.");
-}
-else if (machine == 3 && user == 1){
-	alert("La compu eligió Tijeras, Ganaste!");
+function jugar(){
+	do{
+		var user = prompt("Para empezar a jugar escribe: \n1 para elegir piedra \n2 para elegir papel  \n3 para elegir tijera");
+		var machine = Math.floor(Math.random() * 3) + 1;
+		if (user == machine){
+			alert("EMPATASTE");
+		}
+		else if (machine == 1 && user == 2){
+			alert("¡GANASTE! \nLa compu eligió Piedra");
+				
+		}
+		else if (machine == 1 && user == 3){
+			alert("PERDISTE  \nLa compu eligió Piedra");
+			
+		}
+		else if (machine == 2 && user == 1){
+			alert("PERDISTE. \nLa compu eligió Papel");
+			
+		} 
+		else if (machine == 2 && user == 3){
+			alert("¡GANASTE!  \nLa compu eligió Papel");
+			
+		}
+		else if (machine == 3 && user == 2){
+			alert("PERDISTE  \nLa compu eligió Tijeras");
+			
+		}
+		else if (machine == 3 && user == 1){
+			alert("¡GANASTE! \nLa compu eligió Tijeras");
+			
+		}
+		var volver = prompt("¿Quieres volver a jugar? \nEscribe 'si' para volver a jugar o 'no' para salir.");
+	}
+	while (volver != "no" && volver != "NO" && volver != "No")
 }
