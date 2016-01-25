5957bf2fa1c43d185ac9dbf4f68018e819a2d8b0
hmorv <hugo.moragues@gmail.com>
2016-01-25 22:46:23 +0100

    minor
    optimización switch

--- js.js
+++ js.js
@@ -1,34 +1,37 @@
 
 function jugar(){
-	do{
-		var user = prompt("Para empezar a jugar escribe: \n1 para elegir piedra \n2 para elegir papel  \n3 para elegir tijera");
+	do
+	{
+		var user = parseInt(prompt("Para empezar a jugar escribe: \n1 para elegir piedra \n2 para elegir papel  \n3 para elegir tijera"));
 		var machine = Math.floor(Math.random() * 3) + 1;
 		if (user == machine){
 			alert("EMPATASTE");
 		}
-		else if (machine == 1 && user == 2){
-			alert("¡GANASTE! \nLa compu eligió Piedra");
-				
-		}
-		else if (machine == 1 && user == 3){
-			alert("PERDISTE  \nLa compu eligió Piedra");
-			
-		}
-		else if (machine == 2 && user == 1){
-			alert("PERDISTE. \nLa compu eligió Papel");
-			
-		} 
-		else if (machine == 2 && user == 3){
-			alert("¡GANASTE!  \nLa compu eligió Papel");
-			
-		}
-		else if (machine == 3 && user == 2){
-			alert("PERDISTE  \nLa compu eligió Tijeras");
-			
-		}
-		else if (machine == 3 && user == 1){
-			alert("¡GANASTE! \nLa compu eligió Tijeras");
-			
+		else
+		{
+			switch (user)
+			{
+				case 1:
+				if(machine == 3)
+					alert("¡GANASTE! \nLa compu eligió Tijeras");
+				else
+					alert("PERDISTE. \nLa compu eligió Papel");
+				break;
+				case 2:
+				if(machine == 1)
+					alert("¡GANASTE! \nLa compu eligió Piedra");
+				else
+					alert("PERDISTE  \nLa compu eligió Tijeras");
+				break;
+				case 3:
+				if (machine == 2)
+					alert("¡GANASTE!  \nLa compu eligió Papel");
+				else
+					alert("PERDISTE  \nLa compu eligió Piedra");
+				break;
+				default:
+				break;
+			}
 		}
 		var volver = prompt("¿Quieres volver a jugar? \nEscribe 'si' para volver a jugar o 'no' para salir.");
 	}
